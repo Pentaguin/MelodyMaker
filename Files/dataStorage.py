4891b1de1f0d446bcdfefb75ce75b1c335195bf8
Pentaguin <davidtai171732@gmail.com>
2022-06-08 21:13:24 +0200

    Update dataStorage.py

--- Files/dataStorage.py
+++ Files/dataStorage.py
@@ -5,12 +5,18 @@ from pathlib import Path
 import pickle as pk
 
 class DataStorage:
+    def __init__(self):
+        self.databasePath = Path(os.getcwd()).joinpath("database") # Path to the database directory
+
     def createDatabaseDirectory(self): 
-        if os.path.exists(Path(os.getcwd()).joinpath("database")): 
-            shutil.rmtree(Path(os.getcwd()).joinpath("database")) # Remove the whole tree when the directory already exist
-        
-        os.mkdir('database',0o777) # Create a new directory called database in current path
+        os.mkdir('database',0o777) 
     
+    def checkIfDatabaseDirectoryExist(self):
+        return os.path.exists(self.databasePath) if True else False
+
+    def removeDatabaseDirectory(self):
+        shutil.rmtree(self.databasePath) 
+
     def getPickleFileData(self, fileName):
         file = open(fileName, "rb")
         data = pk.load(file)
@@ -22,6 +28,7 @@ class DataStorage:
         pk.dump(data,file)
         file.close()
     
+
     def createWAVfile(self,fileName, song):
         ps.make_wav (
             song,
